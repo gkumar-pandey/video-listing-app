163b181e0034f6a67bfeb00b2e487fe28091e206
gkumar-pandey <gautamshekhar078@gmail.com>
2023-04-26 18:45:37 +0530

    style: navbar created

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,28 +1,53 @@
-
-import { NavLink } from 'react-router-dom'
-import Container from '../Container/Container'
-import './Header.css'
-import { useLikedVideos } from '../../Context/LikedVideo-context/LikedVideo-context'
+import { NavLink } from "react-router-dom";
+import Container from "../Container/Container";
+import "./Header.css";
+import { useLikedVideos } from "../../Context/LikedVideo-context/LikedVideo-context";
+import { useWatchLaterVideos } from "../../Context/Watchlater-context/WatchLater-context";
 
 const Header = () => {
-    const { likedVideos } = useLikedVideos();
-    return (
-        <div className='header' >
-            <Container>
-                <div className='header-wrapper' >
-                    <div>
-                        <h2 className='logo' >Videos</h2>
-                    </div>
-                    <nav>
-                        <NavLink to={'/'} className='nav-link' >Home</NavLink>
-                        <NavLink to={'/videos'} className='nav-link'  >Videos</NavLink>
-                        <NavLink to={'/likedvideos'} className='nav-link'  >Liked Videos({likedVideos.length}) </NavLink>
-                        <NavLink to={'/watchlater'} className='nav-link'  >Watch Later</NavLink>
-                    </nav>
-                </div>
-            </Container>
-        </div>
-    )
-}
+  const { likedVideos } = useLikedVideos();
+  const { watchLaterVideos } = useWatchLaterVideos();
+  const activeLinkStyle = ({ isActive }: any) => ({
+    backgroundColor: isActive ? "purple" : ""
+  });
 
-export default Header
+  return (
+    <div className="header">
+      <Container>
+        <div className="header-wrapper">
+          <div>
+            <h2 className="logo">Videos</h2>
+          </div>
+          <nav>
+            <NavLink to={"/"} style={activeLinkStyle} className="nav-link">
+              Home
+            </NavLink>
+            <NavLink
+              to={"/videos"}
+              style={activeLinkStyle}
+              className="nav-link"
+            >
+              Videos
+            </NavLink>
+            <NavLink
+              to={"/likedvideos"}
+              style={activeLinkStyle}
+              className="nav-link"
+            >
+              Liked Videos({likedVideos.length}){" "}
+            </NavLink>
+            <NavLink
+              to={"/watchlater"}
+              style={activeLinkStyle}
+              className="nav-link"
+            >
+              Watch Later({watchLaterVideos.length})
+            </NavLink>
+          </nav>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Header;
