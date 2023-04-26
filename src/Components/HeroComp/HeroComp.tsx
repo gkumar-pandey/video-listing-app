4a5439ecd89f42f3c928d30a988333334fe061fd
gkumar-pandey <gautamshekhar078@gmail.com>
2023-04-26 18:49:24 +0530

    style: fix hero

--- src/Components/HeroComp/HeroComp.tsx
+++ src/Components/HeroComp/HeroComp.tsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import './HeroComp.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./HeroComp.css";
+import { Link } from "react-router-dom";
 
 const HeroComp = () => {
-    return (
-        <div>
-            <div className='hero' >
-                <div className='hero-text' >
-                    <h2 className='welcome-text' >Welcome To Neog Video Library</h2>
-                    <div>
-                        <h2 className='text' >To browse all videos , click on the below button or go to the video page</h2>
-                        <Link to={'/videos'} >
-                            <button>Explore Videos</button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="hero">
+        <div className="hero-text">
+          <h2 className="welcome-text">Welcome To Neog Video Library</h2>
+          <div>
+            <h2 className="text">
+              To browse all videos , click on the below button or go to the
+              video page
+            </h2>
+            <Link to={"/videos"}>
+              <button>Explore Videos</button>
+            </Link>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default HeroComp
+export default HeroComp;
